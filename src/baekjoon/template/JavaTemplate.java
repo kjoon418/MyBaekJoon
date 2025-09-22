1c55130670945e67216cb96114cf583bee09c880
Kim, Joon <kjoon418@naver.com>
2025-09-22 14:10:59 +0900

    chore: Java 템플릿 수정

--- src/baekjoon/template/JavaTemplate.java
+++ src/baekjoon/template/JavaTemplate.java
@@ -2,17 +2,16 @@ package baekjoon.template;
 
 import java.io.*;
 
-public class Template {
+/**
+ * 백준 {문제 번호}번 문제 - {문제 이름}({티어})
+ * {분류}
+ */
+public class JavaTemplate {
     
     private static final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     private static final BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        init();
-    }
-
-    private static void init() throws IOException {
 
     }
-
 }
